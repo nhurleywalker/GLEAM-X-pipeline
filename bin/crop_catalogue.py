c31b974df2aee1e66d974c1e9dcc23810ee18bce
Natasha Hurley-Walker <nhw@icrar.org>
2019-05-22 13:35:35 +0800

    Added options to specify the RA and Dec column names

--- bin/crop_catalogue.py
+++ bin/crop_catalogue.py
@@ -18,8 +18,6 @@ parser.add_option('--dec',type="string", dest="dec",
                     help="The declination center of your crop in decimal degrees")
 parser.add_option('--radius',type="float", dest="radius",
                     help="The radius of your crop circle in degrees")
-parser.add_option('--fluxcol',type="string", dest="fluxcol", default="int_flux_wide",
-                    help="The name of the flux density column (default = int_flux_wide)")
 parser.add_option('--minflux',type="float", dest="minflux", default=1.0,
                     help="The minimum flux density of the source (default = 1.0 Jy)")
 parser.add_option('--attenuate', action="store_true", dest="attenuate", default=False,
@@ -28,6 +26,14 @@ parser.add_option('--metafits',type="string", dest="metafits",
                     help="The metafits file for your observation (only necessary if using the beam attenuation option)")
 parser.add_option('--catalogue',type="string", dest="cat",
                     help="The filename of the catalogue you want to read in (fits format).")
+parser.add_option('--racol',type="string", dest="racol", default="RAJ2000",
+                    help="The name of the RA column (in decimal degrees) in your catalogue (default = RAJ2000)")
+parser.add_option('--decol',type="string", dest="decol", default="DEJ2000",
+                    help="The name of your Dec column (in decimal degrees) in your catalogue (default = DEJ2000)")
+parser.add_option('--fluxcol',type="string", dest="fluxcol", default="int_flux_wide",
+                    help="The name of the flux density column (default = int_flux_wide)")
+parser.add_option('--alphacol',type="string", dest="alphacol", default="alpha",
+                    help="The name of the spectral index column (default = alpha)")
 parser.add_option('--output',type="string", dest="output", default="cropped_catalogue.fits",
                     help="The filename of the output cropped catalogue (fits format).")
 (options, args) = parser.parse_args()
@@ -38,15 +44,14 @@ pos = SkyCoord(options.ra, options.dec, unit=(u.deg, u.deg))
 temp = fits.open(options.cat)
 data = temp[1].data
 
-coords = SkyCoord(data["RAJ2000"], data["DEJ2000"], unit=(u.deg, u.deg))
+coords = SkyCoord(data[options.racol], data[options.decol], unit=(u.deg, u.deg))
 separations = np.array(pos.separation(coords))
-
 # Select sources within crop radius of your original RA and Dec
 indices = np.where(separations<options.radius)
 # Select only sources which meet the minimum flux density criterion
 fluxcut = np.where(data[options.fluxcol]>options.minflux)
 # Select only sources with non-zero spectral indices
-alphacut = np.where(np.logical_not(np.isnan(data["alpha"])))
+alphacut = np.where(np.logical_not(np.isnan(data[options.alphacol])))
 indices = np.intersect1d(fluxcut,indices)
 indices = np.intersect1d(alphacut,indices)
 
@@ -54,7 +59,7 @@ indices = np.intersect1d(alphacut,indices)
 
 if options.attenuate:
     if options.metafits:
-        x,y = beam_value(data["RAJ2000"][indices],data["DEJ2000"][indices],options.metafits)
+        x,y = beam_value(data[options.racol][indices],data[options.decol][indices],options.metafits)
 # This might be more correct for Stokes values, but I'm not sure it's what I want...
 #        i = (x**2 + y**2)/2
 # Also, at some point, I would like to save the full Jones matrix into the source model
