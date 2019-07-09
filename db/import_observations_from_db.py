a2ead0897180fc77a44222bad97e9ac2a2016acd
Natasha Hurley-Walker <nhw@icrar.org>
2019-07-09 11:40:57 +0800

    Updated MWA metadata server address

--- db/import_observations_from_db.py
+++ db/import_observations_from_db.py
@@ -10,7 +10,7 @@ import sqlite3
 __author__ = "PaulHancock & Natasha Hurley-Walker"
 
 # Append the service name to this base URL, eg 'con', 'obs', etc.
-BASEURL = 'http://mwa-metadata01.pawsey.org.au/metadata/'
+BASEURL = 'http://ws.mwatelescope.org/metadata/'
 dbfile = '/group/mwasci/nhurleywalker/GLEAM-X-pipeline/db/GLEAM-X.sqlite'
 
 # Function to call a JSON web service and return a dictionary: This function by Andrew Williams
