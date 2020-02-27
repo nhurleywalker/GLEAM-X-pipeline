f0c06dc7e853947b4355ca81b44f1e0f6274ba97
Natasha Hurley-Walker <nhw@icrar.org>
2020-02-27 10:33:49 +0800

    Added fast image test script to quickly see whether an observation is OK

---
+++ bin/obs_testimage.sh
@@ -0,0 +1,122 @@
+#! /bin/bash
+
+usage()
+{
+echo "obs_testimage.sh [-d dep] [-p project] [-q queue] [-t] obsnum
+  -d dep     : job number for dependency (afterok)
+  -q queue   : job queue, default=workq
+  -p project : project, (must be specified, no default)
+  -t         : test. Don't submit job, just make the batch file
+               and then return the submission command
+  obsnum     : the obsid to process" 1>&2;
+exit 1;
+}
+
+# Supercomputer options
+if [[ "${HOST:0:4}" == "zeus" ]]
+then
+    computer="zeus"
+    account="mwasci"
+    standardq="workq"
+    ncpus=28
+#    absmem=60
+#    standardq="gpuq"
+elif [[ "${HOST:0:4}" == "magn" ]]
+then
+    computer="magnus"
+    account="pawsey0272"
+    standardq="workq"
+    ncpus=24
+#    absmem=60
+elif [[ "${HOST:0:4}" == "athe" ]]
+then
+    computer="athena"
+    account="pawsey0272"
+    standardq="gpuq"
+#    absmem=30 # Check this
+fi
+
+scratch="/astro"
+group="/group"
+
+#initial variables
+queue="-p $standardq"
+dep=
+imscale=
+pixscale=
+clean=
+tst=
+# parse args and set options
+while getopts ':td:p:q:' OPTION
+do
+    case "$OPTION" in
+	d)
+	    dep=${OPTARG}
+	    ;;
+	q)
+	    queue="-p ${OPTARG}"
+	    ;;
+    p)
+        project=${OPTARG}
+        ;;
+	t)
+	    tst=1
+	    ;;
+	? | : | h)
+	    usage
+	    ;;
+  esac
+done
+# set the obsid to be the first non option
+shift  "$(($OPTIND -1))"
+obsnum=$1
+
+base="$scratch/mwasci/$USER/$project/"
+code="$group/mwasci/$USER/GLEAM-X-pipeline/"
+
+# if obsid is empty then just print help
+
+if [[ -z ${obsnum} ]] || [[ -z $project ]] || [[ ! -d ${base} ]]
+then
+    usage
+fi
+
+if [[ ! -z ${dep} ]]
+then
+    depend="--dependency=afterok:${dep}"
+fi
+
+# start the real program
+
+script="${code}queue/testimage_${obsnum}.sh"
+cat ${code}/bin/testimage.tmpl | sed -e "s:OBSNUM:${obsnum}:g" \
+                                 -e "s:BASEDIR:${base}:g" \
+                                 -e "s:NCPUS:${ncpus}:g" \
+                                 -e "s:HOST:${computer}:g" \
+                                 -e "s:STANDARDQ:${standardq}:g" \
+                                 -e "s:ACCOUNT:${account}:g" > ${script}
+
+output="${code}queue/logs/testimage_${obsnum}.o%A"
+error="${code}queue/logs/testimage_${obsnum}.e%A"
+
+sub="sbatch --begin=now+15 --output=${output} --error=${error} ${depend} ${queue} ${script}"
+if [[ ! -z ${tst} ]]
+then
+    echo "script is ${script}"
+    echo "submit via:"
+    echo "${sub}"
+    exit 0
+fi
+    
+# submit job
+jobid=($(${sub}))
+jobid=${jobid[3]}
+taskid=1
+
+# rename the err/output files as we now know the jobid
+error=`echo ${error} | sed "s/%A/${jobid}/"`
+output=`echo ${output} | sed "s/%A/${jobid}/"`
+
+echo "Submitted ${script} as ${jobid}. Follow progress here:"
+echo $output
+echo $error
