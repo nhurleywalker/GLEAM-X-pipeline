9711001488da4a107519540b76b394cb1e6b63c8
Natasha Hurley-Walker <nhw@icrar.org>
2020-06-09 15:36:59 +0800

    Use FITS table of NVSS and SUMSS rather than VO table of MRC and VLSSr

--- mosaics/psf_select.py
+++ mosaics/psf_select.py
@@ -3,6 +3,8 @@
 import numpy as np
 import os, sys, re
 
+import astropy
+from astropy.io import fits
 from astropy.io.votable import writeto as writetoVO
 from astropy.table import Table, Column
 from astropy.io.votable import parse_single_table
@@ -12,15 +14,15 @@ from optparse import OptionParser
 usage="Usage: %prog [options]\n"
 parser = OptionParser(usage=usage)
 parser.add_option('--input',dest="input",default=None,
-                  help="Input VO table to downselect.")
+                  help="Input FITS table to downselect.")
 parser.add_option('--minsnr',dest="minsnr",default=10.0,type="float",
                   help="Minimum S/N ratio for sources used to fit PSF (default = 10 sigma)")
-parser.add_option('--nomrc',action="store_false",dest="usemrc",default=True,
-                  help="Don't use MRC (and VLSSr North of Dec 0) to select unresolved sources? (default = use ancillary catalogues.)")
+parser.add_option('--nofilter',action="store_false",dest="usefilter",default=True,
+                  help="Don't use NVSS (and SUMSS South of Dec -40) to select unresolved sources? (default = use NVSS and SUMSS)")
 parser.add_option('--noisolate',action="store_false",dest="isolate",default=True,
                   help="Don't exclude sources near other sources? (will exclude by default)")
 parser.add_option('--output',dest="output",default=None,
-                  help="Output psf vot file -- default is input_psf.vot")
+                  help="Output psf FITS file -- default is input_psfcat.fits")
 (options, args) = parser.parse_args()
 
 # Parse the input options
@@ -34,61 +36,33 @@ else:
 if options.output:
     outputfile=options.output
 else:
-    outputfile=inputfile.replace(".vot","_psf.vot")
+    outputfile=inputfile.replace(".fits","_psfcat.fits")
 
 # Read the VO table and start processing
-if options.isolate:
-    sparse=re.sub(".vot","_isolated.vot",inputfile)
+if options.isolate is True:
+    sparse=re.sub(".fits","_isolated.fits",inputfile)
     os.system('stilts tmatch1 matcher=sky values="ra dec" params=600 \
                action=keep0 in='+inputfile+' out='+sparse)
 else:
     sparse=inputfile
 
-if options.usemrc:
-    catdir=os.environ['MWA_CODE_BASE']+'/MWA_Tools/catalogues'
-    MRCvot=catdir+"/MRC.vot"
-    VLSSrvot=catdir+"/VLSSr.vot"
-# GLEAM: Get rid of crazy-bright sources, really super-extended sources, and sources with high residuals after fit
-    os.system('stilts tpipe in='+sparse+' cmd=\'select ((local_rms<1.0)&&((int_flux/peak_flux)<2)&&((residual_std/peak_flux)<0.1))\' out=temp_crop.vot')
-# MRC: get point like sources (MFLAG is blank)
-    Mmatchvot=re.sub(".vot","_MRC.vot",sparse)
-    os.system('stilts tpipe in='+MRCvot+' cmd=\'select NULL_MFLAG\' cmd=\'addcol PA_MRC "0.0"\' out=mrc_temp.vot')
-# Use only isolated sources
-    os.system('stilts tmatch1 matcher=sky values="_RAJ2000 _DEJ2000" params=600 \
-               action=keep0 in=mrc_temp.vot out=mrc_crop.vot')
-# Match GLEAM with MRC
-    os.system('stilts tmatch2 matcher=skyellipse params=30 in1=mrc_crop.vot in2=temp_crop.vot out=temp_mrc_match.vot values1="_RAJ2000 _DEJ2000 2*e_RA2000 2*e_DE2000 PA_MRC" values2="ra dec 2*a 2*b pa" ofmt=votable')
+if options.usefilter is True:
+    nscat="/group/mwasci/$USER/GLEAM-X-pipeline/models/NVSS_SUMSS_psfcal.fits"
+# Snapshot: Get rid of crazy-bright sources, really super-extended sources, and sources with high residuals after fit
+    os.system('stilts tpipe in='+sparse+' cmd=\'select ((local_rms<1.0)&&((int_flux/peak_flux)<3)&&((residual_std/peak_flux)<0.1))\' out=temp_crop.fits')
+# Match GLEAM with NVSS/SUMSS
+    os.system('stilts tmatch2 matcher=sky params=30 in1='+nscat+' in2=temp_crop.fits out=temp_ns_match.fits values1="RAJ2000 DEJ2000" values2="ra dec" fixcols="dups" suffix1="_ns" suffix2=""')
 # Keep only basic aegean headings
-    os.system('stilts tpipe in=temp_mrc_match.vot cmd=\'keepcols "ra dec peak_flux err_peak_flux int_flux err_int_flux local_rms a err_a b err_b pa err_pa residual_std flags"\' out='+Mmatchvot)
+    os.system('stilts tpipe in=temp_ns_match.fits cmd=\'keepcols "ra dec peak_flux err_peak_flux int_flux err_int_flux local_rms a err_a b err_b pa err_pa residual_std flags"\' out='+outputfile)
 
-# VLSSr: get point-like sources (a and b are < 86", same resolution as MRC); only sources North of Dec +20
-    Vmatchvot=re.sub(".vot","_VLSSr.vot",sparse)
-    os.system('stilts tpipe in='+VLSSrvot+' cmd=\'select ((MajAx<.02389)&&(MinAx<0.02389)&&(_DEJ2000>0)) \' out=vlssr_temp.vot')
-# Use only isolated sources
-    os.system('stilts tmatch1 matcher=sky values="_RAJ2000 _DEJ2000" params=600 \
-               action=keep0 in=vlssr_temp.vot out=vlssr_crop.vot')
-# Match GLEAM with VLSSr
-    os.system('stilts tmatch2 matcher=sky params=120 in1=vlssr_crop.vot in2=temp_crop.vot out=temp_vlssr_match.vot values1="_RAJ2000 _DEJ2000" values2="ra dec" ofmt=votable')
-# Keep only basic aegean headings
-    os.system('stilts tpipe in=temp_vlssr_match.vot cmd=\'keepcols "ra dec peak_flux err_peak_flux int_flux err_int_flux local_rms a err_a b err_b pa_2 err_pa residual_std flags"\' out='+Vmatchvot)
-# Concatenate the MRC and VLSSr matched tables together
-    os.system('stilts tcat in='+Mmatchvot+' in='+Vmatchvot+' out='+outputfile)
+    os.remove('temp_crop.fits')
+    os.remove('temp_ns_match.fits')
 
-    os.remove('temp_crop.vot')
-    os.remove('mrc_temp.vot')
-    os.remove('mrc_crop.vot')
-    os.remove('temp_mrc_match.vot')
-    os.remove('vlssr_temp.vot')
-    os.remove('vlssr_crop.vot')
-    os.remove('temp_vlssr_match.vot')
-    os.remove(Mmatchvot)
-    os.remove(Vmatchvot)
-
-    table = parse_single_table(outputfile)
-    data = table.array
+    hdu = fits.open(outputfile)
+    data = hdu[1].data
 else:
-    table = parse_single_table(sparse)
-    data = table.array
+    hdu = fits.open(sparse)
+    data = hdu[1].data
 
 x=data['ra']
 if max(x)>360.:
@@ -104,7 +78,7 @@ indices=np.where((data['flags']==0) & ((data['peak_flux']/data['local_rms'])>=op
 mask.extend(indices[0])
 mask=np.array(mask)
 
-vot = Table(data[mask])
-vot.description = "Sources selected for PSF calculation."
-writetoVO(vot, outputfile)
+tab = Table(data[mask])
+tab.description = "Sources selected for PSF calculation."
+tab.write(outputfile, overwrite=True)
 
