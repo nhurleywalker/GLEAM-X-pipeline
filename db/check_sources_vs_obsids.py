878d100c57b8bd5a176c708dce4fc57fa8c69858
Tim Galvin <tim.galvin@curtin.edu.au>
2020-08-07 11:09:13 +0800

    mysql changes and selected obsids

--- db/check_sources_vs_obsids.py
+++ db/check_sources_vs_obsids.py
@@ -11,31 +11,73 @@ from populate_sources_table import Source
 from check_src_fov import check_coords
 from beam_value_at_radec import beam_value
 
-__author__ = "Natasha Hurley-Walker"
-
-dbfile = 'GLEAM-X.sqlite'
+__author__ = ["Natasha Hurley-Walker",
+              "Tim Galvin"]
 
 def insert_app(obs_id, source, appflux, infov, cur):
-    cur.execute(""" INSERT OR REPLACE INTO calapparent
+    """Saves the apparent flux of known sources based on the observation pointing
+    direction and the mwa primary beam.
+
+    Arguments:
+        obs_id {int} -- GLEAM-X observation id
+        source {str} -- Name of source to insert
+        appflux {float} -- Brightness of source after applying MWA primary beam
+        infov {bool} -- Is source within the field of view
+        cur {mysql.connector.cursor.MySQLCursor} -- Open cursor to GLEAM-X database
+    """
+    cur.execute("""REPLACE INTO calapparent
                 (obs_id, source, appflux, infov)
                 VALUES (%s, %s, %s, %s); """,
                 (obs_id, source, appflux, infov))
     return
 
 def get_srcs(cur):
-    srciter = cur.execute("""
-    SELECT source, ra, dec, flux, alpha, beta
-    FROM sources""")
-    return srciter
+    """Returns the spectral properties described in the `sources` table.
 
-# NB: Currently selects all observations; may want to finesse this later
-def get_obs(cur):
-    obsiter = cur.execute("""
-    SELECT obs_id, ra_pointing, dec_pointing, cenchan, starttime, delays
-    FROM observation """)
-    return obsiter
+    Arguments:
+        cur {mysql.connector.cursor.MySQLCursor} -- Open cursor to GLEAM-X database
+ 
+    Returns:
+        {List} -- Result set of the executed SQL
+    """
+    srciter = cur.execute("""
+    SELECT source, RAJ2000, DecJ2000, flux, alpha, beta
+    FROM sources""")
+    return cur.fetchall()
+
+def get_obs(cur, obs_id = None):
+    """
+    Return a set of rows from the `observations` table. 
+
+    Arguments:
+        cur {mysql.connector.cursor.MySQLCursor} -- Open cursor to GLEAM-X database
+
+    Keyword Arguments:
+        obs_id {int} -- Limit the returned results to a single `obsid` (default: {None})
+    """
+    if obs_id is None:
+        cur.execute("""SELECT obs_id, ra_pointing, dec_pointing, cenchan, starttime, delays
+        FROM observation """)
+        
+    else:
+        cur.execute(("SELECT obs_id, ra_pointing, dec_pointing, cenchan, starttime, delays "
+                     "FROM observation WHERE obs_id = %s "), 
+                     (obs_id, ))
+
+        
+    return cur.fetchall()
 
 def create_wcs(ra, dec, cenchan):
+    """Creates a fake WCS header to generate a MWA primary beam response
+
+    Arguments:
+        ra {float} -- Observation pointing direction in degrees
+        dec {float} -- Observation pointing direction in degrees
+        cenchan {float} -- Observation frequency in Hertz
+
+    Returns:
+        {astropy.wcs.WCS} -- WCS information describing the observation
+    """
     pixscale = 0.5 / float(cenchan)
     w = wcs.WCS(naxis=2)
     w.wcs.crpix = [4000,4000]
@@ -51,11 +93,25 @@ if __name__ == "__main__":
     conn = mdb.connect()
     cur = conn.cursor()
 
+    # TODO: Add a proper argument parser
+    if len(sys.argv) == 1:
+        print 'Getting all obsids from the user database...'
+        obsids = get_obs(cur)
+        print '...{0} obsids found'.format(len(obsids))
+    else:
+        print 'Reading obsids from {0}...'.format(sys.argv[1])
+        obsids = [int(i.strip()) for i in open(sys.argv[1], 'r')]
+        print '...{0} obsids found'.format(len(obsids))
+
+        # This could be done with one call with a change to the SQL WHERE IN
+        obsids = [list(get_obs(cur, obs_id=i))[0] for i in obsids]
+
+
     srclist = []
     for row in get_srcs(cur):
         srclist.append(Source(row[0],SkyCoord(row[1],row[2],unit=u.deg),row[3],row[4],row[5]))
 
-    for row in get_obs(cur):
+    for row in obsids:
         w = create_wcs(row[1],row[2],row[3])
         t = Time(int(row[4]), format='gps')
         freq = 1.28 * row[3]
@@ -66,7 +122,9 @@ if __name__ == "__main__":
             appflux = src.flux * ( (freq / 150.)**(src.alpha) ) * (xx+yy)/2.
             if np.isnan(appflux):
                 appflux = 0.0
+            
             print row[0], src.name, appflux, check_coords(w, src.pos)
-            insert_app(row[0], src.name, appflux, check_coords(w, src.pos), cur)
+            
+            insert_app(row[0], src.name, float(appflux), check_coords(w, src.pos), cur)
     conn.commit()
     conn.close()
