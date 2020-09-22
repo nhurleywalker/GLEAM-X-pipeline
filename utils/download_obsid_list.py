a3b376aa706381db18e51b7186f1119cfc51b860
Tim Galvin <tim.galvin@curtin.edu.au>
2020-09-22 09:27:24 +0800

    typo fixes

--- utils/download_obsid_list.py
+++ utils/download_obsid_list.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     parser.add_argument('-d','--date', default=None, help='Obsids on this date are returned. Date is expected in YYY-MM-DD format')
     parser.add_argument('-o', '--out', default=None, help='Output path of a line delimited set of obsids. Only supports text output file and ignores file extension. ')
     parser.add_argument('-c','--cen-chan', default=None, type=int, choices=[ 69,  93, 121, 145, 169], help='Obsids matching the specified cenchan are returned. ')
-    parser.add_argument('-i','--hour-angle', default=None, type=int, choices=[ -1, 0, -1], help='Specifies the hour-angle of the obsids to be returned. ')
+    parser.add_argument('-i','--hour-angle', default=None, type=int, choices=[ -1, 0, 1], help='Specifies the hour-angle of the obsids to be returned. ')
 
     args = parser.parse_args()
 
