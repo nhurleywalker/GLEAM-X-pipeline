4a3e8132ccc60d3ecc1c0a5d7e54105e65998dcd
Tim Galvin <tim.galvin@curtin.edu.au>
2020-10-30 18:03:38 +0800

    missing pb lookup to bind in singularity

--- bin/obs_image.sh
+++ bin/obs_image.sh
@@ -107,7 +107,7 @@ chmod 755 "${script}"
 # sbatch submissions need to start with a shebang
 echo '#!/bin/bash' > ${script}.sbatch
 echo 'module load singularity' >> ${script}.sbatch
-echo "singularity run -B '${GXHOME}:${HOME}' ${GXCONTAINER} ${script}" >> ${script}.sbatch
+echo "singularity run -B '${GXHOME}:${HOME}' -B '${GXMWALOOKUP}:/pb_lookup' ${GXCONTAINER} ${script}" >> ${script}.sbatch
 
 sub="sbatch --export=ALL --account=${account} --time=12:00:00 --mem=${GXABSMEMORY}G -M ${GXCOMPUTER} --output=${output} --error=${error}"
 sub="${sub} ${GXNCPULINE} ${GXTASKLINE} ${jobarray} ${depend} ${queue} ${script}.sbatch"
