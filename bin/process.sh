bc50a04a549e0209731cdee46733ce5035496b98
Tim Galvin <tim.galvin@curtin.edu.au>
2020-11-05 10:13:36 +0800

    removed account parameter in process.sh/chain

--- bin/process.sh
+++ bin/process.sh
@@ -5,17 +5,14 @@
 usage()
 {
 echo "process.sh [-p project] [-a account] obsnum
-  -a account : computing account, default pawsey0272
   -p project : project, (must be specified, no default)
   -c         : will attempt to perform cottering on files (default: False)
   obsnum     : the obsid to process" 1>&2;
 exit 1;
 }
 
-pipeuser=$(whoami)
+pipeuser="${GXUSER}"
 
-scratch="/astro"
-group="/group"
 cotter=0
 
 # parse args and set options
@@ -40,25 +37,21 @@ done
 shift  "$(($OPTIND -1))"
 obsnum=$1
 
-
-base="$scratch/mwasci/$pipeuser/$project/"
-code="$group/mwasci/$pipeuser/GLEAM-X-pipeline/"
-script="${code}queue/process_${obsnum}.sh"
+script="${GXSCRIPT}/process_${obsnum}.sh"
 
 if [[ -z ${obsnum} ]] || [[ -z $project ]]
 then
     usage
 fi
 
-if [[ -z ${account} ]]
+if [[ ! -z ${GXACCOUNT} ]]
 then
-    account=pawsey0272
+    account="--account=${GXACCOUNT}"
 fi
 
 
-cat ${code}/bin/chain.tmpl | sed -e "s:OBSNUM:${obsnum}:g" \
+cat ${GXBASE}/templates/chain.tmpl | sed -e "s:OBSNUM:${obsnum}:g" \
                                  -e "s:PROJECT:${project}:g" \
-                                 -e "s:ACCOUNT:${account}:g" \
                                  -e "s:COTTER:${cotter}:g" \
                                   > ${script}
 
