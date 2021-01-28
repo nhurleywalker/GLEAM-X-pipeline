82f569e516d46e0e5a10e3259095a19834ee3fba
Tim Galvin <tgalvin@curtin.edu.au>
2021-01-28 14:32:47 +0800

    fixed bug that would always flag last row/column, slight rewrite
    Changed things up to explicitly set the min/max variables, which could
    not be if the first row/column was valid.

--- gleam_x/bin/fits_trim.py
+++ gleam_x/bin/fits_trim.py
@@ -17,59 +17,64 @@ from astropy.io import fits
 import sys
 
 
-def main(fin, fout):
-    """
-	"""
-    hdulist = fits.open(fin)
-    data = hdulist[0].data
-    # turn pixels that are identically zero, into masked pixels
-    data[np.where(data == 0.0)] = np.nan
+    def trim(fin, fout):
+        """Searches for the four directions (top, bottom, left, right) for the first
+        valid row or column, where valid means not made up entirely of NaNs. Essentially
+        performs a crop to remove any row or column made up entirely of nan pixels. 
 
-    print(f"Input image shape: {data.shape}")
+        Arguments:
+            fin (str) -- Path to the input fits file with dimensions to crop
+            fout (str) -- Path to new output fits file with cropped dimensions
+        """
+        hdulist = fits.open(fin)
+        data = hdulist[0].data
+        # turn pixels that are identically zero, into masked pixels
+        data[np.where(data == 0.0)] = np.nan
 
-    imin, imax = 0, data.shape[1] - 1
-    jmin, jmax = 0, data.shape[0] - 1
-    # select [ij]min/max to exclude rows/columns that are all zero
-    for i in range(0, imax):
-        if np.all(np.isnan(data[:, i])):
+        print(f"Input image shape: {data.shape}")
+
+        imin, imax = 0, data.shape[1] - 1
+        jmin, jmax = 0, data.shape[0] - 1
+
+        # select [ij]min/max to exclude rows/columns that are all zero
+
+        for i in range(0, imax):
             imin = i
-        else:
-            break
+            if not np.all(np.isnan(data[:, i])):
+                break
+        print(f"imin: {imin}")
 
-    print(f"imin: {imin}")
-    for i in range(imax, imin, -1):
-        if np.all(np.isnan(data[:, i])):
+        for i in range(imax, imin, -1):
             imax = i
-        else:
-            break
+            if not np.all(np.isnan(data[:, i])):
+                break
+        print(f"imax: {imax}")
 
-    print(f"imax: {imax}")
-    for j in range(0, jmax):
-        if np.all(np.isnan(data[j, :])):
+        for j in range(0, jmax):
             jmin = j
-        else:
-            break
+            if not np.all(np.isnan(data[j, :])):
+                break
+        print(f"jmin: {jmin}")
 
-    print(f"jmin: {jmin}")
-    for j in range(jmax, jmin, -1):
-        if np.all(np.isnan(data[j, :])):
+        for j in range(jmax, jmin, -1):
             jmax = j
-        else:
-            break
+            if not np.all(np.isnan(data[j, :])):
+                break
 
-    print(f"jmax: {jmax}")
+        print(f"jmax: {jmax}")
 
-    hdulist[0].data = data[jmin:jmax, imin:imax]
+        # End index is not inclusive
+        hdulist[0].data = data[jmin:(jmax+1), imin:(imax+1)]
 
-    print(f"Output data shape: {hdulist[0].data.shape}")
+        print(f"Output data shape: {hdulist[0].data.shape}")
 
-    # recenter the image so the coordinates are correct.
-    hdulist[0].header["CRPIX1"] -= imin
-    hdulist[0].header["CRPIX2"] -= jmin
+        # recenter the image so the coordinates are correct.
+        hdulist[0].header["CRPIX1"] -= imin
+        hdulist[0].header["CRPIX2"] -= jmin
 
-    # save
-    hdulist.writeto(fout, overwrite=True)
-    print("wrote", fout)
+        # save
+        hdulist.writeto(fout, overwrite=True)
+        print("wrote", fout)
 
 
 if __name__ == "__main__":
@@ -80,5 +85,5 @@ if __name__ == "__main__":
     else:
         print("usage: python {0} infile.fits outfile.fits".format(__file__))
         sys.exit()
-    main(fin, fout)
+    trim(fin, fout)
 
