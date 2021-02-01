1c2c4f005b503090213d5c3233cd431f60f15dbb
Tim Galvin <tgalvin@curtin.edu.au>
2021-02-01 18:49:45 +0800

    limiting resources

--- bin/obs_uvsub.sh
+++ bin/obs_uvsub.sh
@@ -101,7 +101,13 @@ chmod 755 "${script}"
 echo '#!/bin/bash' > ${script}.sbatch
 echo "singularity run ${GXCONTAINER} ${script}" >> ${script}.sbatch
 
-sub="sbatch --begin=now+5minutes --export=ALL  --time=02:00:00 --mem=${GXABSMEMORY}G -M ${GXCOMPUTER} --output=${output} --error=${error}"
+if [ ! -z ${GXNCPULINE} ]
+then
+    # autoflag only needs a single CPU core
+    GXNCPULINE="--ntasks-per-node=1"
+fi
+
+sub="sbatch --begin=now+5minutes --export=ALL  --time=02:00:00 --mem=50G -M ${GXCOMPUTER} --output=${output} --error=${error}"
 sub="${sub} ${GXNCPULINE} ${account} ${GXTASKLINE} ${jobarray} ${depend} ${queue} ${script}.sbatch"
 if [[ ! -z ${tst} ]]
 then
