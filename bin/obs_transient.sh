0f65fbc4ab77b7269f299a4837f8dea29b833313
Tim Galvin <tgalvin@curtin.edu.au>
2021-02-24 11:02:04 +0800

    initial commit of new transient task

---
+++ bin/obs_transient.sh
@@ -0,0 +1,149 @@
+#! /bin/bash
+
+usage()
+{
+echo "obs_transient.sh [-d dep] [-p project] [-a account] [-z] [-t] obsnum
+  -d dep     : job number for dependency (afterok)
+  -p project : project, (must be specified, no default)
+  -z         : Debugging mode: image the CORRECTED_DATA column
+                instead of imaging the DATA column
+  -t         : test. Don't submit job, just make the batch file
+               and then return the submission command
+  obsnum     : the obsid to process, or a text file of obsids (newline separated). 
+               A job-array task will be submitted to process the collection of obsids. " 1>&2;
+exit 1;
+}
+
+pipeuser="${GXUSER}"
+
+#initial variables
+dep=
+tst=
+debug=
+# parse args and set options
+while getopts ':tzd:a:p:' OPTION
+do
+    case "$OPTION" in
+	d)
+	    dep=${OPTARG}
+	    ;;
+    a)
+        account=${OPTARG}
+        ;;
+    p)
+        project=${OPTARG}
+        ;;
+    z)
+        debug=1
+        ;;
+	t)
+	    tst=1
+	    ;;
+	? | : | h)
+	    usage
+	    ;;
+  esac
+done
+# set the obsid to be the first non option
+shift  "$(($OPTIND -1))"
+obsnum=$1
+
+queue="-p ${GXSTANDARDQ}"
+base="${GXSCRATCH}/$project"
+code="${GXBASE}"
+
+# if obsid is empty then just print help
+
+if [[ -z ${obsnum} ]] || [[ -z $project ]] || [[ ! -d ${base} ]]
+then
+    usage
+fi
+
+if [[ ! -z ${dep} ]]
+then
+    if [[ -f ${obsnum} ]]
+    then
+        depend="--dependency=aftercorr:${dep}"
+    else
+        depend="--dependency=afterok:${dep}"
+    fi
+fi
+
+if [[ ! -z ${GXACCOUNT} ]]
+then
+    account="--account=${GXACCOUNT}"
+fi
+
+# Establish job array options
+if [[ -f ${obsnum} ]]
+then
+    numfiles=$(wc -l "${obsnum}" | awk '{print $1}')
+    jobarray="--array=1-${numfiles}"
+else
+    numfiles=1
+    jobarray=''
+fi
+
+# start the real program
+
+script="${GXSCRIPT}/transient_${obsnum}.sh"
+cat "${GXBASE}/templates/transient.tmpl" | sed -e "s:OBSNUM:${obsnum}:g" \
+                                 -e "s:BASEDIR:${base}:g" \
+                                 -e "s:DEBUG:${debug}:g" \
+                                 -e "s:PIPEUSER:${pipeuser}:g" > "${script}"
+
+output="${GXLOG}/transient_${obsnum}.o%A"
+error="${GXLOG}/transient_${obsnum}.e%A"
+
+if [[ -f ${obsnum} ]]
+then
+   output="${output}_%a"
+   error="${error}_%a"
+fi
+
+chmod 755 "${script}"
+
+# sbatch submissions need to start with a shebang
+echo '#!/bin/bash' > ${script}.sbatch
+echo "singularity run ${GXCONTAINER} ${script}" >> ${script}.sbatch
+
+sub="sbatch --begin=now+5minutes --export=ALL  --time=4:00:00 --mem=${GXABSMEMORY}G -M ${GXCOMPUTER} --output=${output} --error=${error}"
+sub="${sub} ${GXNCPULINE} ${account} ${GXTASKLINE} ${jobarray} ${depend} ${queue} ${script}.sbatch"
+if [[ ! -z ${tst} ]]
+then
+    echo "script is ${script}"
+    echo "submit via:"
+    echo "${sub}"
+    exit 0
+fi
+    
+# submit job
+jobid=($(${sub}))
+jobid=${jobid[3]}
+
+echo "Submitted ${script} as ${jobid} . Follow progress here:"
+
+for taskid in $(seq ${numfiles})
+    do
+    # rename the err/output files as we now know the jobid
+    obserror=$(echo "${error}" | sed -e "s/%A/${jobid}/" -e "s/%a/${taskid}/")
+    obsoutput=$(echo "${output}" | sed -e "s/%A/${jobid}/" -e "s/%a/${taskid}/")
+
+    if [[ -f ${obsnum} ]]
+    then
+        obs=$(sed -n -e "${taskid}"p "${obsnum}")
+    else
+        obs=$obsnum
+    fi
+
+    if [ "${GXTRACK}" = "track" ]
+    then
+        # record submission
+        ${GXCONTAINER} track_task.py queue --jobid="${jobid}" --taskid="${taskid}" --task='transient' --submission_time="$(date +%s)" \
+                            --batch_file="${script}" --obs_id="${obs}" --stderr="${obserror}" --stdout="${obsoutput}"
+    fi
+
+    echo "$obsoutput"
+    echo "$obserror"
+done
+
